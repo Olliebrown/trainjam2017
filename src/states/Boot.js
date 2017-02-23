a94c1ac62f18efacb1975981bba0dac26eb4763a
Anthony Finch <actionspeakslouder@gmail.com>
2017-02-23 12:59:58 -0600

    Removes fonts

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -9,12 +9,13 @@ export default class extends Phaser.State {
   }
 
   preload () {
-    WebFont.load({
-      google: {
-        families: ['Nunito']
-      },
-      active: this.fontsLoaded
-    })
+    // WebFont.load({
+    //   google: {
+    //     // families: ['Nunito']
+    //   },
+    //   active: this.fontsLoaded
+    // })
+    this.fontsReady = true
 
     let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
