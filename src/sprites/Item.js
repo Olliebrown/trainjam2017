dc560764a8ae28240a9a630543766af7d41949fb
Seth Berrier <berriers@uwstout.edu>
2017-02-24 08:34:24 -0700

    Item Button WIP

--- src/sprites/Item.js
+++ src/sprites/Item.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser'
 import Glow from '../filters/Glow'
 
-export default class extends Phaser.Sprite {
+export class Item extends Phaser.Sprite {
 
   constructor ({ game, x, y, tile }) {
     super(game, x, y, 'sewer-sprites', tile.index - 1)
@@ -11,11 +11,11 @@ export default class extends Phaser.Sprite {
     this.fixedToCamera = true
     this.scale.setTo(0.45, 0.45)
     this.game.physics.arcade.enable(this)
-    this.inMicrowave = false;
+    this.inMicrowave = false
 
     this.name = tile.properties.name
     this.description = tile.properties.description
-    this.filters = [ new Glow(game) ]
+//    this.filters = [ new Glow(game) ]
 
     console.info('Picked up ' + this.name + ' with index ' + tile.index)
   }
@@ -25,6 +25,20 @@ export default class extends Phaser.Sprite {
 
   mouseOn(x, y){
     return this.body.hitTest(x, y);
+  }
+
+}
+
+export class ItemButton extends Phaser.Group {
+
+  constructor({ game, x, y, index }) {
+    super(game)
+
+    let closeBtn = game.make.Button(game, x, y, 'close-btn-sheet', this, this.onClose, 2, 0, 1, 0)
+    this.add(closeBtn)
+
+    let number = game.make.text(game, x, y, index, { font: 'Courier' })
+    this.add(number)
   }
 
 }
