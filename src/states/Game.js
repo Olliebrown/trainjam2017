5ef8da587b9c8c0cd922a11dfc17638e36f8f763
Anthony Finch <actionspeakslouder@gmail.com>
2017-02-23 19:43:37 -0600

    Cleans up enemy generation WIP

--- src/states/Game.js
+++ src/states/Game.js
@@ -14,8 +14,6 @@ export default class extends Phaser.State {
     this.game.physics.startSystem(Phaser.Physics.ARCADE)
     this.tilemap = this.game.add.tilemap('game')
 
-    this.enemy_spawns_triggers = new Phaser.Group(this.game)
-
     this.game.world.setBounds(0, 0, this.tilemap.widthInPixels, this.tilemap.heightInPixels)
 
     this.tilemap.addTilesetImage('sewer-tiles')
@@ -24,14 +22,11 @@ export default class extends Phaser.State {
     this.bg_layer = this.tilemap.createLayer('bg')
     this.sewer_layer = this.tilemap.createLayer('sewer')
     this.enemy_spawns_layer = this.tilemap.createLayer('enemy_spawns')
-    // this.enemy_spawns_layer.visible = false
+    this.enemy_spawns_layer.visible = false
 
 
     this.tilemap.setCollisionByExclusion([0], true, 'sewer')
-    // this.tilemap.setCollisionByExclusion([0], true, 'enemy_spawns')
     this.tilemap.setTileIndexCallback([65], this.generateEnemy, this, 'enemy_spawns')
-
-    this.loadEnemyTriggers()
 
     this.musicIntro = this.game.add.audio('BGM-intro')
     this.musicLoop = this.game.add.audio('BGM-loop')
@@ -58,30 +53,16 @@ export default class extends Phaser.State {
 
     // camera
     this.game.camera.follow(this.player)
-    console.log(this.enemy_spawns_triggers)
   }
 
-  loadEnemyTriggers() {
-    var tiles = this.enemy_spawns_layer.getTiles(0, 0, this.tilemap.widthInPixels, this.tilemap.heightInPixels)
-    for (var t in tiles) {
-      var tile = tiles[t]
-      if (tile.canCollide == true) {
-        console.log(tile)
-        // var trigger = new Phaser.Sprite(this.game, tile.x, tile.y, 'player')
-        this.enemy_spawns_triggers.add(tile)
-      }
-    }
-  }
-
-  generateEnemy () {
+  generateEnemy (player, tile) {
     console.log("Generating enemy?")
   }
 
   update () {
     this.game.physics.arcade.collide(this.player, this.sewer_layer)
-    // this.game.physics.arcade.overlap(this.player, this.enemy_spawns_triggers, this.generateEnemy, null, this)
-
     this.game.physics.arcade.collide(this.player, this.enemy_spawns_layer)
+
     if (this.keys.left.isDown) {
       this.player.body.velocity.x = -PLAYER_SPEED
     } else if (this.keys.right.isDown) {
