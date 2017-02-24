3dbf3f82c198f8cb754be5295c8ae77444ea3a39
Seth Berrier <berriers@uwstout.edu>
2017-02-24 07:52:07 -0700

    Quick fix to remove Fonts
    Removed the lingering fonts stuff!

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -1,5 +1,4 @@
 import Phaser from 'phaser'
-import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
@@ -9,13 +8,6 @@ export default class extends Phaser.State {
   }
 
   preload () {
-    // WebFont.load({
-    //   google: {
-    //     // families: ['Nunito']
-    //   },
-    //   active: this.fontsLoaded
-    // })
-    this.fontsReady = true
 
     let text = this.add.text(this.world.centerX, this.world.centerY, 'loading fonts', { font: '16px Arial', fill: '#dddddd', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
